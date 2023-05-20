add06f09157e458ec56ee4164382af806c7c1499
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-05-20 18:35:46 +0200

    Cards,productName,productPrice,onPress

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Products.css";
 
 export default function Products() {
+  // ovde se samo renderuju i prikazuju sve kartice
   return (
     <div>
       <h1>Produkti</h1>
